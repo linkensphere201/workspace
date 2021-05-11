01bcc73aa2c54a971cb252a9b705116665bf057d
linkensphere201 <11865548+linkensphere201@users.noreply.github.com>
2021-05-11 12:01:20 +0800

    add dl-helper

---
+++ vim-settings/dl-helper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+dirbase=/home/weijian.yang/vim-plugins
+git clone https://github.com/flazz/vim-colorschemes.git ${dirbase}/vim-colorschemes
+git clone https://github.com/rhysd/vim-clang-format.git ${dirbase}/vim-clang-format
+git clone https://github.com/bfrg/vim-cpp-modern.git ${dirbase}/vim-cpp-modern
+git clone https://github.com/vim-airline/vim-airline.git ${dirbase}/vim-airline
+git clone https://github.com/vim-airline/vim-airline-themes.git ${dirbase}/vim-airline-themes
+git clone https://github.com/luochen1990/rainbow.git ${dirbase}/rainbow
+git clone https://github.com/preservim/nerdtree.git ${dirbase}/nerdtree
+git clone https://github.com/pseewald/vim-anyfold.git ${dirbase}/vim-anyfold
+git clone https://github.com/arecarn/vim-fold-cycle.git ${dirbase}/vim-fold-cycle
+git clone https://github.com/vim-scripts/DrawIt.git ${dirbase}/DrawIt
+git clone https://github.com/preservim/tagbar.git ${dirbase}/tagbar
+git clone https://github.com/LucHermitte/lh-vim-lib.git ${dirbase}/lh-vim-lib
+git clone https://github.com/LucHermitte/VimFold4C.git ${dirbase}/VimFold4C
