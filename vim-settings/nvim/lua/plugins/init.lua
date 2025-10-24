cb38898189730ee0463d028a434d208c4e18a0dc
linkensphere201 <linkensphere201>
2025-10-25 04:18:30 +0800

    add profile for nvim

---
+++ vim-settings/nvim/lua/plugins/init.lua
@@ -0,0 +1,12 @@
+return {
+	{ 'neoclide/coc.nvim', branch = 'release' },
+	{ 'nvim-mini/mini.nvim', version = false },
+	{ 'nvim-treesitter/nvim-treesitter', branch = 'master', lazy = false, build = ":TSUpdate"},
+	{ 'preservim/nerdtree', branch = 'master' },
+	{ 'vim-airline/vim-airline', branch = 'master' },
+	{ 'vim-airline/vim-airline-themes', branch = 'master' },
+	{ 'dstein64/vim-win', branch = 'master' },
+	{ 'pseewald/vim-anyfold', branch = 'master' },
+	{'flazz/vim-colorschemes', branch = 'master' },
+	{ 'linkensphere201/vim-set-colors-and-highlights', branch = 'main' },
+}
